4b9966fc250bb7b820d1ecc9114014e6b86392c0
Mark Nagibin <myrkwill@icloud.com>
2022-02-19 22:13:27 +0300

    Initial Commit

---
+++ Shared/SwiftUIProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectsApp.swift
+//  Shared
+//
+//  Created by Mark Nagibin on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
