54e4e042b85c8b8f1f797a744935383fab0506cb
Mark Nagibin <myrkwill@icloud.com>
2022-02-23 23:11:36 +0300

    Add tetris colors

--- Shared/Color.swift
+++ Shared/Color.swift
@@ -10,7 +10,24 @@ import SwiftUI
 extension Color {
 
 	/// QuizGame
-	static var lightblue: Color { .init(red: 173, green: 216, blue: 230) }
-	static var deepSkyBlue: Color { .init(red: 0, green: 191, blue: 255) }
+	public static let lightblue = Color(red: 173, green: 216, blue: 230)
+	public static let deepSkyBlue = Color(red: 0, green: 191, blue: 255)
 
+	/// Tetris
+	public static let tetrisLightBlue = Color(red: 0/255, green: 230/255, blue: 254/255)
+	public static let tetrisDarkBlue = Color(red: 1/255, green: 1/255, blue: 240/255)
+	public static let tetrisGreen = Color(red: 0/255, green: 241/255, blue: 0/255)
+	public static let tetrisYellow = Color(red: 255/255, green: 222/255, blue: 0/255)
+	public static let tetrisOrange = Color(red: 255/255, green: 115/255, blue: 8/255)
+	public static let tetrisRed = Color(red: 255/255, green: 16/255, blue: 60/255)
+	public static let tetrisPurple = Color(red: 184/255, green: 2/255, blue: 253/255)
+	public static let tetrisBlack = Color(red: 0/255, green: 0/255, blue: 0/255)
+
+	public static let tetrisLightBlueShadow = Color(red: 21/255, green: 56/255, blue: 63/255)
+	public static let tetrisDarkBlueShadow = Color(red: 1/255, green: 0/255, blue: 57/255)
+	public static let tetrisGreenShadow = Color(red: 22/255, green: 59/255, blue: 11/255)
+	public static let tetrisYellowShadow = Color(red: 63/255, green: 57/255, blue: 12/255)
+	public static let tetrisOrangeShadow = Color(red: 60/255, green: 31/255, blue: 8/255)
+	public static let tetrisRedShadow = Color(red: 58/255, green: 10/255, blue: 16/255)
+	public static let tetrisPurpleShadow = Color(red: 41/255, green: 3/255, blue: 60/255)
 }
